234208509f14821d4af17e77bb4c7a99cddae2bf
Kristen Mills <kristen@kristen-mills.com>
2015-11-19 13:06:30 -0500

    Some stuff copied from other repos

---
+++ app/js/actions/auth.js
@@ -0,0 +1,57 @@
+'use strict';
+
+import api from '../api';
+
+export const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
+export const SIGN_IN_FAILURE = 'SIGN_IN_FAILURE';
+export const SIGN_OUT_SUCCESS = 'SIGN_OUT_SUCCESS';
+export const SIGN_OUT_FAILURE = 'SIGN_OUT_FAILURE';
+
+function signInSuccess(user) {
+  return {
+    type: SIGN_IN_SUCCESS,
+    user,
+  };
+}
+
+function signInFailed(error) {
+  return {
+    type: SIGN_IN_FAILURE,
+    error,
+  };
+}
+
+function signOutSuccess() {
+  return {
+    type: SIGN_OUT_SUCCESS,
+  };
+}
+
+function signOutFailed(error) {
+  return {
+    type: SIGN_OUT_FAILURE,
+    error,
+  };
+}
+
+export function signIn(googleUser) {
+  return dispatch => {
+    const info = {
+      token: googleUser.getAuthResponse().id_token,
+      id: googleUser.getBasicProfile().getEmail().split('@')[0],
+    };
+    return api.Auth.getToken('google', info.id,  info.token)
+      .then(user => dispatch(signInSuccess(user)))
+      .catch(error => dispatch(signInFailed(error)));
+  };
+}
+
+
+export function signOut() {
+  return dispatch => {
+    return Promise
+      .all([api.Auth.signOut(), gapi.auth2.getAuthInstance().signOut()])
+      .then(() => dispatch(signOutSuccess()))
+      .catch(error => dispatch(signOutFailed(error)));
+  };
+}
--- app/js/store.js
+++ app/js/store.js
@@ -2,7 +2,7 @@
 
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import reducer from './reducers';
+import reducer from './reducers/auth';
 
 const createStoreWithMiddleware = applyMiddleware(
   thunk
